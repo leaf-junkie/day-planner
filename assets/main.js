9e39186f0dd05b243f420e548c854538af8938a6
leaf-junkie <ragsdale.jar@gmail.com>
2020-02-02 14:01:52 -0500

    Add functionality:
    - Use Moment to determine current time and whether a given time slot is in the past, present, or future
    - Add on load event listener and refresh button to display current status of time slots

--- assets/main.js
+++ assets/main.js
@@ -1,45 +1,92 @@
-// display the current day
-var weekDay = moment().format("dddd");
-var currentDate = moment().format("dddd MMM D, YYYY");
-var currentTime = moment().format("h:mm a");
-var timeSlot = $(".timeSlot").text();
+$(document).ready(function() {
 
-$("#currentDate").text(currentDate);
-$("#currentTime").text(currentTime);
+    // display the current day
+    var weekDay = moment().format("dddd");
+    var currentDate = moment().format("dddd MMM D, YYYY");
+    var currentTime = moment().format("h:mm a");
+    var timeSlot = $(".timeSlot").text();
+    var formattedTimeSlot = parseInt(timeSlot);
+    var currentHour = moment().format("H"); 
+    var timeArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    // var inputField;
+    // var note;
 
-// Each hour should be color coded to reflect whether the time slot is in the past,
-// present, or future. This will change depending on the time of day.
-// currentTime - timeSlot 
-for (i = 0; i < 9; i++) {
-    if (currentTime - timeSlot < 0) {
-        // time is in the past
-    }
-    else if (currentTime - timeSlot > 0) {
-        // time is in the future
-    }
-    else if (currentTime - timeSlot === 0) {
-        // time is in the present
-    }
-    console.log(currentTime - timeSlot)
-}
+    console.log(formattedTimeSlot - currentHour);
 
-// Clicking on the save button will store the time and user input in localStorage.
-$(".saveBtn").on("click", function(event) {
-    event.preventDefault();
+    $("#currentDate").text(currentDate);
+    $("#currentTime").text(currentTime);
 
-    var note = $(".note").val();
+    function getStoredNotes() {
+        var inputField = $(event.target).prev("input").data("input");
+        var note = $(event.target).prev("input").val();
 
-    for (i = 0; i < 9; i++) {
+        console.log(inputField);
         console.log(note);
+
+        for (var i = 0; i < localStorage.length; i++) {
+            console.log("stored: " + localStorage.getItem(inputField, note));
+        }
     }
+    // Each hour should be color coded to reflect whether the time slot is in the past,
+    // present, or future. This will change depending on the time of day.
+
+    function determineRelevantTime() {
+        
+        timeArr.forEach(hour => {
+
+            var rowInput = $(".list-group input");
+            // console.log(rowInput);
+
+            // determine whether a time slot is in the past, future, or present
+            if (hour - currentHour < 0) {
+                // console.log(hour + " - past")
+                rowInput.addClass("timePast");
+            }
+            else if (hour - currentHour > 0) {
+                // console.log(hour + " - future")
+                rowInput.addClass("timeFuture");
+
+            }
+            else if (hour - currentHour === 0) {
+                // console.log(hour + " - present");
+                rowInput.addClass("timePresent");
+            }
+        });
+    }
+
+    $(document).on("load", function(event) {
+        determineRelevantTime();
+        getStoredNotes();
+    });
+
+    $("#refreshBtn").on("click", function(event) {
+        event.preventDefault();
+        determineRelevantTime();
+        getStoredNotes();
+    });
+
+    // Clicking on the save button will store the time and user input in localStorage.
+    $(".saveBtn").on("click", function(event) {
+        event.preventDefault();
+
+        // check whether each slot is past, present, or future
+        // and update if necessary
+        determineRelevantTime();
+
+        // input and relevant note
+        inputField = $(event.target).prev("input").data("input");
+        note = $(event.target).prev("input").val();
+
+        // save in local storage 
+        localStorage.setItem(inputField, note);
+    });
+
+    // Bonus features //////////////////////////////////////////////////////////////
+
+    // TODO: User may select background image for their planner
+    // When the user hovers over the bg image, a button appears providing the option
+    // to change their bg image. On click, the user is shown a list of available images
+
+    // TODO: enable lights mode/dark mode
 
 });
-
-
-// Bonus features //////////////////////////////////////////////////////////////
-
-// TODO: User may select background image for their planner
-// When the user hovers over the bg image, a button appears providing the option
-// to change their bg image. On click, the user is shown a list of available images
-
-// TODO: enable lights mode/dark mode
